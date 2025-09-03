27e7aa97f9b5fe4d6460bb711c12318b4cafd3bc
Elin <elin@mikira.se>
2025-09-03 19:24:06 +0200

    Removed unused code

--- Problems/Dailies/September/02-WaysToPlacePeople/WaysToPlacePeopleSolver.cs
+++ Problems/Dailies/September/02-WaysToPlacePeople/WaysToPlacePeopleSolver.cs
@@ -3,25 +3,6 @@ namespace LeetCodeProblems.Dailies.WaysToPlacePeople;
 public class WaysToPlacePeopleSolver
 {
     /// <summary>
-    /// Check if point a is the upper left of a rectangle defined by points a and b, while making sure that
-    /// point a is not the same as point b
-    /// </summary>
-    private static bool IsUpperLeft(int[] a, int[] b) => a[0] <= b[0] && a[1] >= b[1] &&
-                                                        (a[0] != b[0] || a[1] != b[1]);
-
-    /// <summary>
-    /// Check if the rectangle defined by points a and b contains point c including edges, while making sure that
-    /// point c is not the same as point a or point b
-    /// </summary>
-    /// <param name="a">The upper left point</param>
-    /// <param name="b">The lower right point</param>
-    /// <param name="c">The point to check if it is contained within the rectangle</param>
-    private static bool ContainsPoint(int[] a, int[] b, int[] c) => a[0] <= c[0] && b[0] >= c[0] &&
-                                                                a[1] >= c[1] && b[1] <= c[1] &&
-                                                                (a[0] != c[0] || a[1] != c[1]) &&
-                                                                (b[0] != c[0] || b[1] != c[1]);
-
-    /// <summary>
     /// Find the number of points which make up the upper left corner of a rectangle defined by any other point 
     /// in the list which is not itself, such that no other points in the list are contained within the rectangle. 
     /// 
