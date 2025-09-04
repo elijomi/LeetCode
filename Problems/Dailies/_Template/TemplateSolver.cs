080893cf8bf5fac98003c832efc051008ea95800
Elin <elin@mikira.se>
2025-09-04 08:23:25 +0200

    Add TemplateRunner and TemplateSolver classes for problem implementation

---
+++ Problems/Dailies/_Template/TemplateSolver.cs
@@ -0,0 +1,9 @@
+namespace LeetCodeProblems.Dailies.Template;
+
+public class TemplateSolver
+{
+    public int SomeProblemSolution(int[][] a)
+    {
+        throw new NotImplementedException();
+    }
+}
