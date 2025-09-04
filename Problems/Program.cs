bb05d5a034f85e4e14a078933289113d0f26e34d
Elin <elin@mikira.se>
2025-09-04 10:16:47 +0200

    Add GenerateParentheses implementation

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,4 +2,5 @@
 //LeetCodeProblems.Dailies.MaxAveragePassRatio.MaxAveragePassRatioRunner.Run();
 //LeetCodeProblems.Dailies.WaysToPlacePeople.WaysToPlacePeopleRunner.Run();
 //LeetCodeProblems.Dailies.FindClosestPerson.FindClosestPersonRunner.Run();
-LeetCodeProblems.Top100Liked.LetterCombinationsOfAPhoneNumber.Runner.Run();
+//LeetCodeProblems.Top100Liked.LetterCombinationsOfAPhoneNumber.Runner.Run();
+LeetCodeProblems.Top100Liked.GenerateParentheses.Runner.Run();
---
+++ Problems/Top100Liked/GenerateParentheses.cs
@@ -0,0 +1,65 @@
+
+namespace LeetCodeProblems.Top100Liked.GenerateParentheses;
+
+/// <summary>
+/// https://leetcode.com/problems/generate-parentheses
+/// </summary>
+public class Runner
+{
+    public static void Run()
+    {
+        var solver = new Solution();
+
+        // Test cases
+        var tests = new (int n, IList<string> expected)[]
+        {
+            (3, new List<string> { "((()))","(()())","(())()","()(())","()()()" }),
+            (1, new List<string> { "()" }),
+            (2, new List<string> { "(())", "()()" }),
+            (4, new List<string> { "(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()" }),
+            (0, new List<string> { "" })
+        };
+
+        int i = 1;
+        foreach (var (n, expected) in tests)
+        {
+            var actual = solver.GenerateParenthesis(n);
+            Console.WriteLine($"Test {i}: expected=[{string.Join(",", expected)}], actual=[{string.Join(",", actual)}] - {(actual.SequenceEqual(expected) ? "PASS" : "FAIL")}");
+            i++;
+        }
+    }
+}
+
+/// <summary>
+/// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+/// 
+/// Constraints:
+/// 1 <= n <= 8
+/// </summary>
+public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        GenerateParenthesisRecursive(result, string.Empty, 0, 0, n);
+        return result;
+    }
+
+    private void GenerateParenthesisRecursive(IList<string> result, string current, int open, int close, int max)
+    {
+        if (current.Length == max * 2)
+        {
+            result.Add(current);
+            return;
+        }
+
+        if (open < max)
+        {
+            GenerateParenthesisRecursive(result, current + "(", open + 1, close, max);
+        }
+        if (close < open)
+        {
+            GenerateParenthesisRecursive(result, current + ")", open, close + 1, max);
+        }
+    }
+}
