a40419be4900224039a7d44b56b17132ac860161
Elin <elin@mikira.se>
2025-09-08 09:24:38 +0200

    Add Find Unique Ints to Zero implementation (Sep 7th 2025 LeetCode Daily)

---
+++ Problems/Dailies/September/07-FindUniqueIntsToZero/FindUniqueIntsToZeroRunner.cs
@@ -0,0 +1,30 @@
+namespace LeetCodeProblems.Dailies.FindUniqueIntsToZero;
+
+/// <summary>
+/// https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero
+/// </summary>
+public static class Runner
+{
+    public static void Run()
+    {
+        var solver = new Solution();
+
+        var tests = new[] { 5, 3, 1 };
+
+        int i = 1;
+        foreach (var n in tests)
+        {
+            var actual = solver.SumZero(n);
+
+            int sum = 0;
+            for (int j = 0; j < actual.Length; j++)
+            {
+                sum += actual[j];
+            }
+
+            var actualDisplay = $"[" + string.Join(", ", actual) + "]";
+            Console.WriteLine($"Test {i}: n={n}, sum={sum}, actual={actualDisplay} - {(sum == 0 ? "PASS" : "FAIL")}");
+            i++;
+        }
+    }
+}
